cb74cb5d37f270e2f7f271983645820e61640488
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-03 14:37:14 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,7 @@
 def hello_t(array)
   if block_given?
     i = 0 
+    
     while i < array.length 
       yield array[i]
       i = i + 1 
@@ -9,6 +10,7 @@ def hello_t(array)
     array
   else 
     puts "Hey! No block was given!"
+  end
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
